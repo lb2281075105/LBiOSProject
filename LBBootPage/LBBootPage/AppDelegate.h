70784aa9d6ac6e91b7f4e5d642023e8bb5ded711
lb2281075105 <2281075105@qq.com>
2018-03-08 00:14:46 +0800

    初始化引导页

---
+++ LBBootPage/LBBootPage/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LBBootPage
+//
+//  Created by liubo on 2018/3/8.
+//  Copyright © 2018年 刘博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LBBootPage/LBBootPage/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LBBootPage
+//
+//  Created by liubo on 2018/3/8.
+//  Copyright © 2018年 刘博. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
