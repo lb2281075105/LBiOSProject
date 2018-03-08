0c3dd409807b5d12e196d0145f0b6150738542bb
lb2281075105 <2281075105@qq.com>
2018-03-08 21:41:08 +0800

    提交

--- LBBootPage/LBBootPage/引导页/LBFirstPageController.m
+++ LBBootPage/LBBootPage/引导页/LBFirstPageController.m
@@ -23,7 +23,8 @@
 @property (nonatomic, strong) UILabel *nameLabel;
 @property (nonatomic, strong) UILabel *englishLabel;
 @property (nonatomic, strong) UIImageView *imageView;
-
+@property (nonatomic, strong) UIButton *imageViewButton;
+@property (nonatomic, strong) UIImageView *imageView2;
 @end
 
 @implementation LBFirstPageController
@@ -49,11 +50,14 @@
     _pageControl = [[UIPageControl alloc]init];
     _pageControl.numberOfPages = 4;
     _pageControl.currentPage = 0;
-    _pageControl.frame = CGRectMake((WIDTH - 8) / 2.0, HEIGHT - 35, 8, 8);
     _pageControl.pageIndicatorTintColor = [UIColor redColor];// 设置非选中页的圆点颜色
     _pageControl.currentPageIndicatorTintColor = [UIColor yellowColor]; // 设置选中页的圆点颜色
     [self.view addSubview:_pageControl];
-    
+    [_pageControl mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.centerX.equalTo(_imageView);
+        make.height.width.equalTo(@8);
+        make.bottom.equalTo(@(-25));
+    }];
     
     _timer =  [NSTimer scheduledTimerWithTimeInterval:3.0 target:self selector:@selector(function) userInfo:nil repeats:YES];
     [NSTimer scheduledTimerWithTimeInterval:3 repeats:NO block:^(NSTimer * _Nonnull timer) {
@@ -71,6 +75,27 @@
 
     _nameLabel.textAlignment = NSTextAlignmentCenter;
     _englishLabel.textAlignment = NSTextAlignmentCenter;
+    
+    // 图片
+    UIImageView *imageView1 = [[UIImageView alloc]init];
+    [self.view addSubview:imageView1];
+    imageView1.image = [UIImage imageNamed:@"游艇"];
+    [imageView1 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(@332);
+        make.width.equalTo(@120);
+        make.height.equalTo(@39);
+        make.left.equalTo(@70);
+    }];
+    
+    _imageView2 = [[UIImageView alloc]init];
+    [self.view addSubview:_imageView2];
+    _imageView2.image = [UIImage imageNamed:@"城市剪影"];
+    [_imageView2 mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.top.equalTo(@242);
+        make.width.equalTo(@(WIDTH));
+        make.height.equalTo(@100);
+        make.left.equalTo(@(0));
+    }];
     
     [self.view addSubview:_nameLabel];
     [self.view addSubview:_englishLabel];
@@ -92,16 +117,84 @@
     static int i = 0;
     i++;
     _pageControl.currentPage = i;
-    
+
     if (i == 1) {
         _nameLabel.text = @"飞机";
         _englishLabel.text = @"Plane";
+        // 游艇
+        UIImageView *imageView2 = [[UIImageView alloc]init];
+        [self.view addSubview:imageView2];
+        imageView2.image = [UIImage imageNamed:@"游艇"];
+        [imageView2 mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(@332);
+            make.width.equalTo(@(120));
+            make.height.equalTo(@39);
+            make.left.equalTo(@(70));
+        }];
+        // 城市
+        _imageView2 = [[UIImageView alloc]init];
+        [self.view addSubview:_imageView2];
+        _imageView2.image = [UIImage imageNamed:@"城市剪影"];
+        [_imageView2 mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(@242);
+            make.width.equalTo(@(WIDTH));
+            make.height.equalTo(@100);
+            make.left.equalTo(@(-70));
+        }];
+        // 云
+        UIImageView *imageView4 = [[UIImageView alloc]init];
+        [self.view addSubview:imageView4];
+        imageView4.image = [UIImage imageNamed:@"2.2"];
+        [imageView4 mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(@180);
+            make.width.equalTo(@(130));
+            make.height.equalTo(@70);
+            make.right.equalTo(@(-38));
+        }];
+
     }else if (i == 2){
         _nameLabel.text = @"汽车";
         _englishLabel.text = @"Car";
+        _imageView2 = [[UIImageView alloc]init];
+        [self.view addSubview:_imageView2];
+        _imageView2.image = [UIImage imageNamed:@"高架桥"];
+        [_imageView2 mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(@284);
+            make.width.equalTo(@(WIDTH));
+            make.height.equalTo(@43);
+            make.left.equalTo(@(0));
+        }];
+        UIImageView *imageView5 = [[UIImageView alloc]init];
+        [self.view addSubview:imageView5];
+        imageView5.image = [UIImage imageNamed:@"汽车剪影"];
+        [imageView5 mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.top.equalTo(@263);
+            make.width.equalTo(@(68));
+            make.height.equalTo(@23);
+            make.right.equalTo(@(110));
+        }];
+
     }else if (i == 3){
         _nameLabel.text = @"游艇联盟";
         _englishLabel.text = @"Yacht Alliance";
+        _imageViewButton = [[UIButton alloc]init];
+        [_imageViewButton setImage:[UIImage imageNamed:@"圆角矩形 1"] forState:(UIControlStateNormal)];
+        [self.view addSubview:_imageViewButton];
+        [_imageViewButton mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.height.equalTo(@46);
+            make.width.equalTo(@168);
+            make.bottom.equalTo(@(-60));
+            make.centerX.equalTo(_imageView);
+        }];
+        UILabel *lijiLabel = [[UILabel alloc]init];
+        lijiLabel.text = @"立即开始";
+        lijiLabel.font = [UIFont boldSystemFontOfSize:18];
+        lijiLabel.textColor = [UIColor whiteColor];
+        [_imageViewButton addSubview:lijiLabel];
+        [lijiLabel mas_makeConstraints:^(MASConstraintMaker *make) {
+            make.center.equalTo(_imageViewButton);
+
+        }];
         [_timer invalidate];
         _timer = nil;
     }
